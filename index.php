7302f86240a5cde1f8a28c92403db48a6a2e3027
FURKAN ILGIN <furkanilgin@gmail.com>
2014-02-05 01:05:47 +0200

    xml file is found which is defined in page-configuration.xml file

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
-include "view/".$_GET["page"].".php";
+include "./libs/XmlToHtmlConverter.php";
+
+echo XmlToHtmlConverter::convert($_GET["page"]);
+
+//include "view/".$_GET["page"].".php";
 
 ?>
---
+++ libs/XmlToHtmlConverter.php
@@ -0,0 +1,25 @@
+<?php
+
+class XmlToHtmlConverter{
+
+	private $xhtml;
+
+	public static function convert($page){
+		
+		$confStr = file_get_contents("./conf/page-configuration.xml");
+		$root = simplexml_load_string($confStr);
+
+		$pageDefinitionCount = 0;
+		foreach($root as $pageNode){
+			if($pageNode["name"] == $page){
+				if($pageDefinitionCount > 0){
+					throw new Exception('There must be only one page definition with same name in page-configuration.xml');
+				}
+				$xhtml = $pageNode["view"];
+				$pageDefinitionCount++;
+			}
+		}
+		
+		echo $xhtml; // fount xml file
+	}
+}
