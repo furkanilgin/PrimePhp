30b553a832ff199946f2d83093bf84eb5fedafa1
FURKAN ILGIN <furkanilgin@gmail.com>
2014-02-05 13:24:47 +0200

    xml is parsed, assignin to objects

--- libs/XmlToHtmlConverter.php
+++ libs/XmlToHtmlConverter.php
@@ -1,10 +1,14 @@
 <?php
 
+require_once("./libs/components/Dropdown.php");
+
 class XmlToHtmlConverter{
 
 	private $xhtml;
 
 	public static function convert($page){
+		
+		/** Find xml file path */
 		
 		$confStr = file_get_contents("./conf/page-configuration.xml");
 		$root = simplexml_load_string($confStr);
@@ -15,11 +19,24 @@ class XmlToHtmlConverter{
 				if($pageDefinitionCount > 0){
 					throw new Exception('There must be only one page definition with same name in page-configuration.xml');
 				}
-				$xhtml = $pageNode["view"];
+				$xhtmlFileName = $pageNode["view"];
 				$pageDefinitionCount++;
 			}
 		}
 		
-		echo $xhtml; // found xml file
+		/** Find xml tags and assign them to html element classes */
+		
+		$xmlStr = file_get_contents("./view/".$xhtmlFileName);
+		$root = simplexml_load_string($xmlStr);
+		
+		$tagClassMapping = array(
+			"dropdown" => "Dropdown"
+		);
+		
+		foreach($root as $pageNode){
+			$componentArray[] = new $tagClassMapping[$pageNode->getName()];
+		}
+		
+		print_r($componentArray);
 	}
 }
